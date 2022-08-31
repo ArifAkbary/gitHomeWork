74fbe58c057d0ee3218f6831c7e32c3c774b8b71
ArifAkbary <arifakbary022@gmail.com>
2022-08-31 14:42:46 -0700

    add employee steps done

--- src/test/java/steps/pageInitilize.java
+++ src/test/java/steps/pageInitilize.java
@@ -1,12 +1,20 @@
 package steps;
 
+import pages.DashbordPage;
 import pages.LoginPage;
+import pages.addEmployeePage;
 
 public class pageInitilize {
     public static LoginPage login;
+    public static DashbordPage dash;
+
+    public static addEmployeePage addEmplyee;
 
     public static void initializePageObjects(){
         login = new LoginPage();
+        dash=new DashbordPage();
+        addEmplyee=new addEmployeePage();
+
 
     }
 
---
+++ target/test-classes/features/addEmployee.feature
@@ -0,0 +1,9 @@
+Feature: This feature is going to add employee in HRMS application
+
+
+
+Scenario: add employee
+When user clicks on PIM option
+And user clicks on add employee option
+When user enters firstName, middleName and lastName
+And user clicks on save button
